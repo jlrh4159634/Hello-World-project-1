e4c71cb11ce762f4257cf99531d79267d0b3af18
jlrh4159634 <44813976+jlrh4159634@users.noreply.github.com>
2019-01-08 10:16:23 -0700

    Add files via upload

---
+++ ForLoop.py
@@ -0,0 +1,15 @@
+# print()
+# for w in range(1, 101):
+#     print('{0:3d}'.format(w))
+#
+#
+#
+#
+# for u in range (100, 10001,500):
+#     print ('{0:5d}'.format(u))
+
+
+for row in range (1,10):
+    for col in range (1,5):
+        print(row,col,'  ',end='')
+    print()
