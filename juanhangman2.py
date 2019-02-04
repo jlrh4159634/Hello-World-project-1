52a17f5756c616768bf40f8fd837b6bc5e57aa8a
jlrh4159634 <44813976+jlrh4159634@users.noreply.github.com>
2019-02-04 10:38:27 -0700

    Add files via upload

--- juanhangman2.py
+++ juanhangman2.py
@@ -1,3 +1,7 @@
+import random
+from juanhangmanlist import text_w
+
+
 
 
 def gallows(penal):
@@ -94,27 +98,35 @@ def gallows(penal):
             '   ========= \n'
     return gal
 
-word= 'hangman'
-secret_word= list(word)
-print(secret_word)
-new_word=list=['*' for i in range(len(secret_word))]
-print(new_word)
 
-penalty=0
-letters_used=[]
-while"*" in new_word and penalty <7:
-    guess=input('Enter a letter\n')
-    if guess not in secret_word or guess in letters_used:
-        penalty+=1
-    else:
-        for i in range(len(secret_word)):
-            if guess==secret_word[i]:
-                new_word[i]=secret_word[i]
-    letters_used.append(guess)
-    print('Penalty', penalty)
-    print(gallows(penalty))
+again = 'y'
+while again == 'y':
 
-    print(letters_used)
+    index=random.randint(0, len(text_w))
+    word = text_w[index]
+    secret_word = list(word)
+    print(secret_word)
+    new_word = ['*' for i in range(len(secret_word))]
     print(new_word)
-print('The End')
+
+    penalty=0
+    letters_used=[]
+    while"*" in new_word and penalty <7:
+        guess=input('Enter a letter\n')
+        if guess not in secret_word or guess in letters_used:
+            penalty+=1
+        else:
+            for i in range(len(secret_word)):
+                if guess==secret_word[i]:
+                    new_word[i]=secret_word[i]
+        letters_used.append(guess)
+        print('Penalty', penalty)
+        print(gallows(penalty))
+
+        print(letters_used)
+        print(new_word)
+    print('The End')
+    again = input('Enter y if you want to play another game, else enter n \n')
+
+print("Good bye, see you next time!")
 exit()
